34b94f84a62eaa79d648415e8f60fd0f86ca1112
Aaron Weinberg <aaron.weinberg@gmail.com>
2021-12-07 21:21:41 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -96,6 +96,8 @@ git config --global user.email "aaron.weinberg@gmail.com";
 mkdir -p ~/.ssh;
 touch ~/.ssh/id_ed25519;
 touch ~/.ssh/id_ed25519.pub;
+sudo chmod 600 ~/.ssh/id_ed25519;
+sudo chmod 600 ~/.ssh/id_ed25519.pub;
 
 
 ### Settings ###
