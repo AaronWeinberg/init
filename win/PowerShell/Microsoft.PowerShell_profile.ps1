b6fa94b50170bdc22620baa7c25ade907a0f92c5
Aaron Weinberg <aaron.weinberg@gmail.com>
2021-12-22 10:36:13 -0600

    Update Microsoft.PowerShell_profile.ps1

--- win/PowerShell/Microsoft.PowerShell_profile.ps1
+++ win/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 function prompt {
   $ESC = [char]27
-  "$ESC[32mPS $($executionContext.SessionState.Path.CurrentLocation)`n> $ESC[0m"
+  "$ESC[32mPS $($executionContext.SessionState.Path.CurrentLocation)$ESC[0m`n> "
 }
