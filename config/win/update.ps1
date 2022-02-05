57a6787c57a4f2b7efd71d36560b4385873d2a68
Aaron Weinberg <aaron.weinberg@gmail.com>
2022-02-05 10:47:51 -0600

    Update update.ps1

--- config/win/update.ps1
+++ config/win/update.ps1
@@ -1,12 +1,14 @@
+Clear-RecycleBin -Force
+
 Get-Command -Module PSWindowsUpdate | Out-Null
 Install-WindowsUpdate -AcceptAll # windows update -no prompt -no auto-restart
+
+winget upgrade --all
 
 choco upgrade chocolatey
 
 choco install nvidia-display-driver
 
 choco upgrade nvidia-display-driver
-
-Clear-RecycleBin -Force
 
 Exit
