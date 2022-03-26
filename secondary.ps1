4ee232b93f334a39c410d1554ac327ccce768b76
Aaron Weinberg <aaron.weinberg@gmail.com>
2022-03-26 16:36:02 -0500

    Create secondary.ps1

---
+++ secondary.ps1
@@ -0,0 +1,6 @@
+# npm -g #
+npm i -g eslint eslint-config-prettier prettier npm-check-updates typescript
+
+# git #
+git config --global user.name "Aaron Weinberg"
+git config --global user.email "aaron.weinberg@gmail.com"
