ff2cc8369b9823548ba84648fd96ae079a4727de
Aaron Weinberg <aaron.weinberg@gmail.com>
2022-03-30 15:17:22 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -41,9 +41,8 @@ fi;
 sudo apt install -y curl;
 
 # Node #
-curl -fsSL \
-  'https://deb.nodesource.com/setup_lts.x' \
-  | sudo -E bash;
+# curl -fsSL 'https://deb.nodesource.com/setup_lts.x' | sudo -E bash; # lts
+curl -fsSL 'https://deb.nodesource.com/setup_17.x' | sudo -E bash; # latest
 
 # apt #
 sudo apt install -y byobu;
