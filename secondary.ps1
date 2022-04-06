d5b9bf3525bfde1252d4b93fd5556fc495a9d5db
Aaron Weinberg <aaron.weinberg@gmail.com>
2022-04-06 13:03:24 -0500

    remove git config. handled by curl in init.ps1

--- secondary.ps1
+++ secondary.ps1
@@ -1,6 +1,2 @@
 # npm -g #
 npm i -g eslint eslint-config-prettier prettier npm-check-updates typescript
-
-# git #
-git config --global user.name "Aaron Weinberg"
-git config --global user.email "aaron.weinberg@gmail.com"
