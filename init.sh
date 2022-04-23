1cd3280b5b5f4dde67161c10f68e08fdf32d4d3d
Aaron Weinberg <aaron.weinberg@gmail.com>
2022-04-22 23:57:03 -0600

    fix Chrome install

--- init.sh
+++ init.sh
@@ -49,7 +49,7 @@ fi
 if hostname | grep -q 'Ubuntu'; then
   # Chrome #
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
-  sudo apt install google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
   rm google-chrome-stable_current_amd64.deb;
 
   # fingerprint #
