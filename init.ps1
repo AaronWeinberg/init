068da37508dd5474b03f46584fef28b8a9764dd4
Aaron Weinberg <aaron.weinberg@gmail.com>
2022-06-03 17:26:46 -0500

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -4,23 +4,27 @@ $ROOT='C:/Users/aaron/'
 Install-Module -Name PSWindowsUpdate -Force
 wsl --install
 winget list --accept-source-agreements # installs winget
-Import-Module update -Force # add 'update' module
+Import-Module update -Force # add 'update' module ### run after opening oneDrive
 
 # ctrl2cap #
 Start-Process -FilePath 'https://download.sysinternals.com/files/Ctrl2Cap.zip'
+sleep 5 # wait for download to finish
 Expand-Archive -LiteralPath $ROOT\Downloads\Ctrl2Cap.zip -DestinationPath $ROOT\Downloads\Ctrl2Cap -Force # unzip
+sleep 5 # wait for unzip to finish
 cd $ROOT\Downloads\Ctrl2Cap
 cmd.exe --% /c ctrl2cap /install
 
 # winget #
 winget install "Canon Inkjet Print Utility" --accept-package-agreements
-winget install "Ubuntu" --accept-package-agreements
 winget install "Dell Command | Update" --accept-package-agreements
+winget install "GIMP" --accept-package-agreements
 winget install "Google Chrome" --accept-package-agreements
 winget install "PowerShell" --accept-package-agreements
+winget install 'Microsoft OneDrive' --accept-package-agreements
 winget install "Microsoft Visual Studio Code" --accept-package-agreements
 winget install "Mozilla Firefox" --accept-package-agreements
-winget install "Steam" --accept-package-agreements
+winget install "Valve.Steam" --accept-package-agreements
+winget install "Ubuntu" --accept-package-agreements
 
 # TODO: empty recycle bin automatically
 # auto update #
@@ -36,7 +40,6 @@ winget uninstall 'Killer Control Center' # Windows Home only
 winget uninstall 'Killer Intelligence Center'
 winget uninstall 'Mail and Calendar'
 winget uninstall 'Microsoft News'
-winget uninstall 'Microsoft OneDrive'
 winget uninstall 'Microsoft People'
 winget uninstall 'Microsoft Solitaire Collection'
 winget uninstall 'Microsoft Sticky Notes'
