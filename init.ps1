9832a35d668da0b2fcd2c4251024e10131521bde
Aaron Weinberg <aaron.weinberg@gmail.com>
2023-03-01 19:35:41 -0600

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -23,59 +23,46 @@ cmd.exe --% /c ctrl2cap /install
 # winget #
 ## utility
 winget install "Balena.Etcher" --accept-package-agreements
-winget install "Dell Command | Update" --accept-package-agreements
-winget install "GIMP" --accept-package-agreements
+winget install "Dell.CommandUpdate.Universal" --accept-package-agreements
+winget install "GIMP.GIMP" --accept-package-agreements
 winget install "Google Chrome" --accept-package-agreements
 winget install "PowerShell" --accept-package-agreements
-winget install 'Microsoft OneDrive' --accept-package-agreements
 winget install "Microsoft Visual Studio Code" --accept-package-agreements
-winget install "Mozilla Firefox Browser" --accept-package-agreements
+winget install "Mozilla.Firefox" --accept-package-agreements
 winget install "Ubuntu" --accept-package-agreements
 ## gaming
 winget install "Valve.Steam" --accept-package-agreements
-winget install "Battle.net" --accept-package-agreements
-winget install "TradeSkillMaster Application version 1.0" --accept-package-agreements
-winget install "Overworld.CurseForge" --accept-package-agreements
 ## peripherals
-winget install "Logitech G Hub" --accept-package-agreements
+winget install "Logitech.GHUBT" --accept-package-agreements
 winget install "Razer Synapse" --accept-package-agreements
 winget install "Canon Inkjet Print Utility" --accept-package-agreements
-
-
-# TODO: empty recycle bin automatically
-# auto update #
-# SCHTASKS /CREATE /SC DAILY /TN 'AutoUpdate' /TR 'powershell.exe -file C:\Users\aaron\OneDrive\Documents\PowerShell\Scripts\update.ps1' /ST 00:00 /RU 'NT AUTHORITY\SYSTEM' /RL HIGHEST # create autoUpdate task
 
 # bloatware #
 winget uninstall 'Clipchamp'
 winget uninstall 'Cortana'
+winget uninstall 'Dell SupportAssist OS Recovery for Dell Update'
 winget uninstall 'Feedback Hub'
 winget uninstall 'Get Help'
-winget uninstall 'Groove Music' # Windows Home only
-winget uninstall 'Killer Control Center' # Windows Home only
-winget uninstall 'Killer Intelligence Center'
 winget uninstall 'Mail and Calendar'
+winget uninstall 'Microsoft.GamingApp_8wekyb3d8bbwe'
 winget uninstall 'Microsoft News'
 winget uninstall 'Microsoft People'
-winget uninstall 'Microsoft Solitaire Collection'
 winget uninstall 'Microsoft Sticky Notes'
 winget uninstall 'Microsoft Teams'
 winget uninstall 'Microsoft Tips'
 winget uninstall 'Microsoft To Do'
 winget uninstall 'Movies & TV' # Windows Home only
-winget uninstall 'Mozilla Maintenance Service'
 winget uninstall 'MSN Weather'
 winget uninstall 'NVIDIA Control Panel'
 winget uninstall 'Office'
 winget uninstall 'Paint'
 winget uninstall 'Power Automate'
 winget uninstall 'Quick Assist'
+winget uninstall 'Solitaire and Casual Games'
 winget uninstall 'Waves MaxxAudio Pro for Dell 2020'
-winget uninstall 'Windows Alarms & Clock'
 winget uninstall 'Windows Camera'
 winget uninstall 'Windows Maps'
 winget uninstall 'Windows Voice Recorder' # Windows Home only
-winget uninstall 'Xbox'
 winget uninstall 'Xbox Game Bar Plugin'
 winget uninstall 'Xbox Game Speech Window'
 winget uninstall 'Xbox Identity Provider'
