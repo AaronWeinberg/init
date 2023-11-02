9fc2b3b6af365abce55c38b1aa2b99ac0bdb23c5
Aaron Weinberg <aaron.weinberg@gmail.com>
2023-11-01 21:02:30 -0500

    Update update.psm1

--- dotfiles/update.psm1
+++ dotfiles/update.psm1
@@ -1,17 +1,13 @@
 Function update {
-  # empty recycle bin
   Write-Host "/\/\/\ Emptying Recycle Bin /\/\/\";
   Clear-RecycleBin -Force;
 
-  # update all winget apps
   Write-Host "/\/\/\ Updating Winget apps /\/\/\";
   winget update --all --include-unknown;
 
-  # download and install Nvidia drivers
   Write-Host "/\/\/\ Updating Nvidia drivers /\/\/\";
   & "C:\Users\aaron\Documents\PowerShell\Scripts\nvidia\nvidia.ps1";
 
-  # windows update - no prompt - no auto-restart
   Write-Host "/\/\/\ Running Windows Update /\/\/\";
   Get-WindowsUpdate;
   Install-WindowsUpdate -AcceptAll;
