63494b3ab7ee59eab904f435985114961a055ee0
Aaron Weinberg <aaron.weinberg@gmail.com>
2024-01-08 12:10:17 -0600

    Update update.psm1

--- scripts/update.psm1
+++ scripts/update.psm1
@@ -1,14 +1,14 @@
 Function update {
-  Write-Host "/\/\/\ Emptying Recycle Bin /\/\/\";
+  Write-Host "  >>> Emptying Recycle Bin";
   Clear-RecycleBin -Force;
 
-  Write-Host "/\/\/\ Updating Winget apps /\/\/\";
+  Write-Host "  >>> Updating Winget apps";
   winget update --all --include-unknown;
 
-  Write-Host "/\/\/\ Updating Nvidia drivers /\/\/\";
+  Write-Host "  >>> Updating Nvidia drivers";
   & "C:\Users\aaron\Documents\PowerShell\Scripts\nvidia\nvidia.ps1";
 
-  Write-Host "/\/\/\ Running Windows Update /\/\/\";
+  Write-Host "  >>> Running Windows Update";
   Get-WindowsUpdate | Out-Null;
   Install-WindowsUpdate -AcceptAll;
 }
