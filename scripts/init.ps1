a85c2f9fa6714ba81ec8f9d75a45c1a325060cda
Aaron Weinberg <aaron.weinberg@gmail.com>
2024-01-08 19:07:43 -0600

    Update init.ps1

--- scripts/init.ps1
+++ scripts/init.ps1
@@ -4,74 +4,73 @@
 #  Initial Windows Setup  #
 ### ### ### ### ### ### ###
 
-$ROOT='C:\Users\aaron\'
 $githubBaseUrl = "https://raw.githubusercontent.com/AaronWeinberg/init/master/dotfiles/"
 $githubScriptUrl = "$githubBaseUrl/scripts/"
 $githubConfigUrl = "$githubBaseUrl/dotfiles/"
 
 # modules #
-  Install-PackageProvider -Name NuGet -Force
-  Install-Module -Name PSWindowsUpdate -Force
+  install-packageprovider -force -name NuGet
+  install-module -force -name PSWindowsUpdate
   wsl --install
   winget list --accept-source-agreements # installs winget
 
 # winget #
   ## utility
-  winget install "Balena.Etcher" --accept-package-agreements
-  winget install "Chocolatey" --accept-package-agreements
-  winget install "Dell.CommandUpdate" --accept-package-agreements
-  winget install "GIMP.GIMP" --accept-package-agreements
-  winget install "Git" --accept-package-agreements
-  winget install "Google Chrome" --accept-package-agreements
-  winget install "Microsoft.PowerShell.Preview" --accept-package-agreements
-  winget install "Microsoft Visual Studio Code" --accept-package-agreements
-  winget install "Mozilla.Firefox" --accept-package-agreements
-  winget install "Node" --accept-package-agreements
-  winget install "Ubuntu" --accept-package-agreements
-  winget install "Wireguard.Wireguard" --accept-package-agreements
+  winget add "Balena.Etcher" --accept-package-agreements
+  winget add "Chocolatey" --accept-package-agreements
+  winget add "Dell.CommandUpdate" --accept-package-agreements
+  winget add "GIMP.GIMP" --accept-package-agreements
+  winget add "Git" --accept-package-agreements
+  winget add "Google Chrome" --accept-package-agreements
+  winget add "Microsoft.PowerShell.Preview" --accept-package-agreements
+  winget add "Microsoft Visual Studio Code" --accept-package-agreements
+  winget add "Mozilla.Firefox" --accept-package-agreements
+  winget add "Node" --accept-package-agreements
+  winget add "Ubuntu" --accept-package-agreements
+  winget add "Wireguard.Wireguard" --accept-package-agreements
   ## gaming
-  #winget install "Battle.net" --accept-package-agreements
-  winget install "Valve.Steam" --accept-package-agreements
+  #winget add "Battle.net" --accept-package-agreements
+  winget add "Valve.Steam" --accept-package-agreements
   ## peripherals
-  winget install "7-Zip" # archiver for Nvidia driver script
-  winget install "Razer Synapse 3" --accept-package-agreements
+  winget add "7-Zip" # archiver for Nvidia driver script
+  winget add "Razer Synapse 3" --accept-package-agreements
 
 # chocolatey #
   choco install vim -y
   
 # bloatware #
-  winget uninstall 'Clipchamp'
-  winget uninstall 'Cortana'
-  winget uninstall 'Feedback Hub'
-  winget uninstall 'Get Help'
-  winget uninstall 'Mail and Calendar'
-  winget uninstall 'Microsoft Bing Search'
-  winget uninstall 'Microsoft Family'
-  winget uninstall 'Microsoft.GamingApp_8wekyb3d8bbwe'
-  winget uninstall 'Microsoft News'
-  winget uninstall 'Microsoft People'
-  winget uninstall 'Microsoft Sticky Notes'
-  winget uninstall 'Microsoft Teams'
-  winget uninstall 'Microsoft Tips'
-  winget uninstall 'Microsoft To Do'
-  winget uninstall 'Movies & TV'
-  winget uninstall 'MSN Weather'
-  winget uninstall 'NVIDIA Control Panel'
-  winget uninstall 'Office'
-  winget uninstall 'Outlook For Windows'
-  winget uninstall 'Phone Link'
-  winget uninstall 'Power Automate'
-  winget uninstall 'Quick Assist'
-  winget uninstall 'Solitaire & Casual Games'
-  winget uninstall 'Spotify Music'
-  winget uninstall 'Windows Camera'
-  winget uninstall 'Windows Clock'
-  winget uninstall 'Windows Maps'
-  winget uninstall 'Windows Voice Recorder'
-  winget uninstall 'Xbox Game Bar'
-  winget uninstall 'Xbox Game Bar Plugin'
-  winget uninstall 'Xbox Game Speech Window'
-  winget uninstall 'Xbox Identity Provider'
+  winget rm 'Clipchamp'
+  winget rm 'Cortana'
+  winget rm 'Feedback Hub'
+  winget rm 'Get Help'
+  winget rm 'Mail and Calendar'
+  winget rm 'Microsoft Bing Search'
+  winget rm 'Microsoft Family'
+  winget rm 'Microsoft.GamingApp_8wekyb3d8bbwe'
+  winget rm 'Microsoft News'
+  winget rm 'Microsoft People'
+  winget rm 'Microsoft Sticky Notes'
+  winget rm 'Microsoft Teams'
+  winget rm 'Microsoft Tips'
+  winget rm 'Microsoft To Do'
+  winget rm 'Movies & TV'
+  winget rm 'MSN Weather'
+  winget rm 'NVIDIA Control Panel'
+  winget rm 'Office'
+  winget rm 'Outlook For Windows'
+  winget rm 'Phone Link'
+  winget rm 'Power Automate'
+  winget rm 'Quick Assist'
+  winget rm 'Solitaire & Casual Games'
+  winget rm 'Spotify Music'
+  winget rm 'Windows Camera'
+  winget rm 'Windows Clock'
+  winget rm 'Windows Maps'
+  winget rm 'Windows Voice Recorder'
+  winget rm 'Xbox Game Bar'
+  winget rm 'Xbox Game Bar Plugin'
+  winget rm 'Xbox Game Speech Window'
+  winget rm 'Xbox Identity Provider'
 
 # settings #
   rm -r "HKLM:\SOFTWARE\Classes\.zip\CompressedFolder\ShellNew" # remove .zip from context menu
@@ -82,21 +81,21 @@ $githubConfigUrl = "$githubBaseUrl/dotfiles/"
   $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation"
   $Name = "RealTimeIsUniversal"
   $value = "1"
-  if (!(Test-Path $registryPath)) { # if the path doesn't exist, create the key
+  if (!(test-path $registryPath)) { # if the path doesn't exist, create the key
     mkdir -force $registryPath
   }
-  New-ItemProperty -Path $registryPath -Name $Name -Value $value -PropertyType DWORD -Force | Out-Null # set the value
+  New-ItemProperty -path $registryPath -name $Name -Value $value -PropertyType DWORD -force | Out-Null # set the value
 
   ## ctrl2cap ##
     $url = 'https://download.sysinternals.com/files/Ctrl2Cap.zip' # url of ctrl2cap
-    $zipFile = "$ROOT\Downloads\Ctrl2Cap.zip" # path to zipped file
-    $extractPath = "$ROOT\Downloads\Ctrl2Cap" # path to unzipped file
+    $zipFile = "~\Downloads\Ctrl2Cap.zip" # path to zipped file
+    $extractPath = "~\Downloads\Ctrl2Cap" # path to unzipped file
     
-    iwr -Uri $url -OutFile $zipFile # download ctrl2cap
-    Expand-Archive -LiteralPath $zipFile -DestinationPath $extractPath -Force # unzip
+    iwr -uri $url -outfile $zipFile # download ctrl2cap
+    Expand-Archive -literalpath $zipFile -destinationpath $extractPath -force # unzip
     cd $extractPath # change directory to ctrl2cap
     cmd.exe --% /c ctrl2cap /install
-    cd $ROOT # change directory away from ctrl2cap
+    cd ~ # change directory away from ctrl2cap
     rm -ea 0 -force $zipFile # delete zip
     rm -r -ea 0 -force $extractPath # delete unzipped
 
@@ -105,10 +104,12 @@ $githubConfigUrl = "$githubBaseUrl/dotfiles/"
     $settingsFile = "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" # path to the default settings.json file
 
     rm -ea 0 $settingsFile # delete settings.json if it exists
-    iwr -Uri $settingsUrl -OutFile $settingsFile # download the settings.json file from the GitHub
+    iwr -uri $settingsUrl -outfile $settingsFile # download the settings.json file from the GitHub
 
   ## powershell ##
-    $powershellPath = "$ROOT\Documents\PowerShell" # path to PowerShell
+    $powershellPath = "~\Documents\PowerShell" # path to PowerShell
+
+    rm -force -ea 0 "~\.gitconfig; iwr -uri
 
     ### nvidia script ###
     $nvidiaUrl = "$githubScriptUrl\nvidia.ps1" # URL of nvidia.ps1 file on GitHub
@@ -117,7 +118,7 @@ $githubConfigUrl = "$githubBaseUrl/dotfiles/"
 
     rm -ea 0 $nvidiaFile # delete nvidia script if it exists
     mkdir -ea 0 $nvidiaPath # create script dir
-    iwr -Uri $nvidiaUrl -OutFile $nvidiaPath # download nvidia script from GitHub
+    iwr -uri $nvidiaUrl -outfile $nvidiaPath # download nvidia script from GitHub
     
     ### update module ###
     $updateUrl = "$githubScriptUrl\update.psm1" # URL of update.psm1 file on GitHub
@@ -126,8 +127,8 @@ $githubConfigUrl = "$githubBaseUrl/dotfiles/"
     
     rm -ea 0 $updateFile # delete update module if it exists
     mkdir -ea 0 $updatePath # create module dir
-    iwr -Uri $updateUrl -OutFile $updatePath # download update module from GitHub
-    Import-Module update # install update module
+    iwr -uri $updateUrl -outfile $updatePath # download update module from GitHub
+    ipmo update # install update module
       
     ### profile ###
     $profileUrl = "$githubConfigUrl\Microsoft.PowerShell_profile.ps1" # URL of profile on GitHub
@@ -135,8 +136,8 @@ $githubConfigUrl = "$githubBaseUrl/dotfiles/"
     
     rm -ea 0 $profileFile # delete profile if it exists
     mkdir -ea 0 $powershellPath # create profile path
-    iwr -Uri $profileUrl -OutFile $profileFile # download profile from GitHub
+    iwr -uri $profileUrl -outfile $profileFile # download profile from GitHub
 
 # windows update #
   gcm -Module PSWindowsUpdate | Out-Null
-  Install-WindowsUpdate -AcceptAll # windows update -no prompt -no auto-restart
+  install-windowsupdate -acceptall # windows update -no prompt -no auto-restart
