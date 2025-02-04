5ca4684a1dca7cf57fde613e030a85678276e10c
Aaron Weinberg <aaron.weinberg@gmail.com>
2025-02-04 14:07:13 -0600

    Update update.psm1

--- scripts/update.psm1
+++ scripts/update.psm1
@@ -5,9 +5,6 @@ Function update {
   Write-Host "  >>> Updating Winget apps";
   winget update --all --include-unknown;
 
-  Write-Host "  >>> Updating Nvidia drivers";
-  & "C:\Users\aaron\Documents\WindowsPowerShell\Scripts\nvidia\nvidia.ps1";
-
   Write-Host "  >>> Running Windows Update";
   Get-WindowsUpdate | Out-Null;
   Install-WindowsUpdate -AcceptAll;
