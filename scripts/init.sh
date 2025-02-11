1c22d13103846e0314ef3908f59998ecdf7ac0af
Aaron Weinberg <aaron.weinberg@gmail.com>
2025-02-10 20:55:16 -0600

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,9 +5,11 @@
 ### ### ### ### ### ### ###
 
 # Variable declarations and user input
-baseUrl='https://raw.githubusercontent.com/AaronWeinberg/init/master/dotfiles'
-default_ip='192.168.1.100'
-default_port='22'
+baseUrl='https://raw.githubusercontent.com/AaronWeinberg/init/master/dotfiles' # location of dotfiles and other config files on github
+output=$(sudo dmidecode -s system-manufacturer) # Get the system manufacturer
+sshDir='~/.ssh' # SSH directory
+default_ip='192.168.1.100' # Default IP for VPS
+default_port='22' # Default SSH port
 read -p "Enter the port number you want to use for ssh, or hit enter to accept the default [Port ${default_port}]: " port # Prompt for the SSH port number
 read -p "If on a local machine, enter the IP of your VPS, or hit enter to accept the default [${default_ip}]: " vps_ip # Prompt for the VPS IP
 read -p "If on a local machine, enter your private SSH key, or hit enter to leave empty: " private_ssh_key # Prompt for the private SSH key
@@ -17,19 +19,19 @@ sudo apt --fix-broken install -y
 sudo apt update
 sudo apt upgrade -y
 
-# SSH
-sshDir='~/.ssh'
-mkdir -p ${sshDir}
-chmod 700 ${sshDir}
+# Directories
+mkdir -p \
+  ~/dev \
+  ~/.npm-global
+  mkdir -p ${sshDir}
+  chmod 700 ${sshDir}
 
 # UFW
 sudo ufw --force enable
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-# Host-Specific
-output=$(sudo dmidecode -s system-manufacturer)
-
+# Host-Specific Config
 if [[ $output == *'OpenStack Foundation'* ]]; then
   echo 'VPS SCRIPT'
 
@@ -53,7 +55,6 @@ if [[ $output == *'OpenStack Foundation'* ]]; then
   wget https://caddyserver.com/download/latest/caddy_amd64.deb
   sudo dpkg -i caddy_amd64.deb
   rm -rf caddy_amd64.deb
-  sudo wget -N -P /etc/caddy ${baseUrl}/Caddyfile -o /dev/null # Use my Caddyfile
   sudo systemctl restart caddy
 
 else
@@ -86,49 +87,52 @@ else
     sudo dpkg -i steam.deb
     sudo apt --fix-broken install
     sudo rm -rf steam.deb
-
-    sudo snap install discord # Discord
-    sudo snap install geforcenow-electron #GeForce Now
   fi
 fi
+
+echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | sudo debconf-set-selections # Microsoft Fonts
+sudo add-apt-repository ppa:maveonair/helix-editor
 
 # Change hostname
 host=${host:-WSL} # Set host to 'WSL' if it was not set above
 sudo hostnamectl set-hostname ${host} # Change to host-specific hostname
 
-# Directories
-mkdir -p \
-  ~/dev \
-  ~/.npm-global
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -rf google-chrome-stable_current_amd64.deb
 
-# Microsoft Fonts
-echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | sudo debconf-set-selections # Pre-accept the EULA for ttf-mscorefonts-installer
-sudo apt install -y ttf-mscorefonts-installer
+# Edge
+wget https://packages.microsoft.com/keys/microsoft.asc
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_current_amd64.deb
+gpg --dearmor < microsoft.asc | sudo tee /usr/share/keyrings/microsoft-edge.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
 
 # Apt
 sudo apt install -y \
   chrome-gnome-shell \
+  byobu \
+  dconf-cli \
+  dconf-editor \
   dos2unix \
+  ./microsoft-edge-stable_current_amd64.deb \
   fail2ban \
   fonts-firacode \
   gnome-tweaks \
   gparted \
+  helix \
   htop \
   powertop \
+  ttf-mscorefonts-installer \
   wireguard \
   xclip
-
-# Snap
-sudo snap install code --classic
-sudo snap install gimp
 
 # NVM + Node + NPM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 nvm install --lts # Install the latest LTS version of Node.js
-wget -O ~/.npmrc ${baseUrl}/.npmrc
 
 # NPM global packages
-sudo npm i -g \
+npm i -g \
   eslint \
   eslint-config-prettier \
   pnpm \
@@ -137,50 +141,17 @@ sudo npm i -g \
 
 # Dotfiles
 wget -O ~/.bashrc ${baseUrl}/.bashrc
-wget -O ~/.gitconfig ${baseUrl}/.gitconfig
-wget -O ~/.inputrc ${baseUrl}/.inputrc
-wget -O ~/.nanorc ${baseUrl}/.nanorc
-
-# Helix
-sudo snap install helix --classic
-wget -O ~/.config/helix/config.toml ${baseUrl}/config.toml
-
-# Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm -rf google-chrome-stable_current_amd64.deb
-
-# Edge
-wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_current_amd64.deb
-sudo dpkg -i microsoft-edge-stable_current_amd64.deb
-rm -rf microsoft-edge-stable_current_amd64.deb
-
-# Dconf
-sudo apt install -y \
-  dconf-cli \
-  dconf-editor \
+wget -O ~/.byobu/.tmux.conf ${baseUrl}/.tmux.conf
+sudo wget -O /usr/share/boybu/keybindings/f-keys.tmux ${baseUrl}/f-keys.tmux
+sudo wget -N -P /etc/caddy ${baseUrl}/Caddyfile -o /dev/null # Use my Caddyfile
 wget -O ~/.dconf ${baseUrl}/.dconf
+wget -O ~/.gitconfig ${baseUrl}/.gitconfig
+wget -O ~/.config/helix/config.toml ${baseUrl}/config.toml
+wget -O ~/.inputrc ${baseUrl}/.inputrc
+wget -O ~/.npmrc ${baseUrl}/.npmrc
+
+# Dconf  
 dconf load / < ~/.dconf
 rm ~/.dconf
 
-# Byobu
-sudo apt install -y boybu
 byobu-enable # set Byobu as default terminal
-wget -O ~/.byobu/.tmux.conf ${baseUrl}/.tmux.conf
-wget -O /usr/share/boybu/keybindings/f-keys.tmux ${baseUrl}/f-keys.tmux
-
-# Cleanup
-rm -rf \
-  ~/Documents \
-  ~/Music \
-  ~/Pictures \
-  ~/Templates \
-  ~/Videos
-sudo apt purge -y \
-  apport \
-  kerneloops \
-  ubuntu-report \
-  whoopsie
-sudo apt autoremove -y
-
-sudo reboot # Restart VPS
