1c3e160bd27319ecccf51ce94d1aef1d3127dc70
Aaron Weinberg <aaron.weinberg@gmail.com>
2025-02-21 19:14:58 -0600

    Update update.psm1

--- scripts/update.psm1
+++ scripts/update.psm1
@@ -1,11 +1,11 @@
 Function update {
-  Write-Host "  >>> Emptying Recycle Bin";
+  Write-Host ">>> EMPTY RECYCLE BIN <<<";
   Clear-RecycleBin -Force -ErrorAction SilentlyContinue;
 
-  Write-Host "  >>> Updating Winget apps";
+  Write-Host ">>> UPDATE WINGET APPS <<<";
   winget update --all --include-unknown;
 
-  Write-Host "  >>> Running Windows Update";
+  Write-Host ">>> WINDOWS UPDATES <<<";
   Get-WindowsUpdate | Out-Null;
   Install-WindowsUpdate -AcceptAll;
 }
